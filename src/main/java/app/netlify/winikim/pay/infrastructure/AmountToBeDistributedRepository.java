7f462b81e363470df470a006776efb8b36a1aac8
winikim <winikim0115>
2020-10-12 18:26:42 +0900

    feat: Add LockMode for receiveDistributedAmount

--- src/main/java/app/netlify/winikim/pay/infrastructure/AmountToBeDistributedRepository.java
+++ src/main/java/app/netlify/winikim/pay/infrastructure/AmountToBeDistributedRepository.java
@@ -2,11 +2,14 @@ package app.netlify.winikim.pay.infrastructure;
 
 import app.netlify.winikim.pay.domain.entity.AmountToBeDistributed;
 import java.util.Optional;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface AmountToBeDistributedRepository extends JpaRepository<AmountToBeDistributed, Long> {
 
   boolean existsByTokenAndRoomId(String token, String roomId);
 
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
   Optional<AmountToBeDistributed>  findByTokenAndRoomId(String token, String roomId);
 }
