11d617a772249267fe7df7e4af7f7ebae2a016d8
winikim <winikim0115>
2020-10-12 19:01:23 +0900

    feat: Add handle MissingRequestHeaderException

--- src/main/java/app/netlify/winikim/pay/exception/GlobalExceptionHandler.java
+++ src/main/java/app/netlify/winikim/pay/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import app.netlify.winikim.pay.domain.exception.NotEnoughAmountException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,6 +20,14 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   ErrorResponse handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+    log.error("[handleMethodArgumentTypeMismatchException] message = {}",
+        e.getMessage());
+    return new ErrorResponse(400, e.getMessage());
+  }
+
+  @ExceptionHandler(MissingRequestHeaderException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  ErrorResponse handleMethodArgumentTypeMismatchException(MissingRequestHeaderException e) {
     log.error("[handleMethodArgumentTypeMismatchException] message = {}",
         e.getMessage());
     return new ErrorResponse(400, e.getMessage());
